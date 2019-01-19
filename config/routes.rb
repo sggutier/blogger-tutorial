7fbfb4add5a490ef37d366cb7c5fb61505bbfe84
Germán Gutiérrez <me@sggc.me>
2019-01-19 14:47:26 -0600

    form-based workflow feature completed (or something like that)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   BloggerTutorial::Application.routes.draw do
+    root to: 'articles#index'
     resources :articles
   end
 end
