fd1b6c117d5bc36886652b8bc763e661253f5857
Germán Gutiérrez <me@sggc.me>
2019-01-20 17:54:46 -0600

    Added way more stuff

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,16 @@
 class Article < ApplicationRecord
+  has_attached_file :image
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]  
+  has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+  def tag_list
+    self.tags.collect{ |tag| tag.name }.join(", ")
+  end
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   BloggerTutorial::Application.routes.draw do
     root to: 'articles#index'
-    resources :articles
+    resources :articles do
+      resources :comments
+    end
+    resources :tags
   end
 end
